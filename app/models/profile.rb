f0e221f756bb97c082c6036bbb39688a283f46b7
Cesar Valderrama <valderramago@gmail.com>
2019-11-21 02:47:42 +0000

    Adds Profile methods to user and profile classes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,9 @@ class Profile < ApplicationRecord
   has_many :roles
 	has_many :users, through: :roles
 	
-	
+	Profile.all.each do |profile|
+	  define_singleton_method profile.name.to_sym do
+  		Profile.where(name: profile.name.to_sym).first
+  	end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,15 @@ class User < ApplicationRecord
   has_many :roles
 	has_many :profiles, through: :roles
 	
-	
+	Profile.all.each do |profile|
+    if profile.name == "admin"
+      define_method "#{profile.name}?" do
+  			return profiles.where(name: profile.name).any?
+  		end
+  	else
+  	  define_method "#{profile.name}?" do
+  	    return profiles.where(name: profile.name).any? || admin?
+  	  end
+  	end
+  end
 end
