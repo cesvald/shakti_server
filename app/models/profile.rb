669e4b369d5ad30282a06d4130aa296460d26036
Cesar Valderrama <valderramago@gmail.com>
2019-11-21 02:39:03 +0000

    Removes not existing tables while migrating

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,5 @@ class Profile < ApplicationRecord
   has_many :roles
 	has_many :users, through: :roles
 	
-	Profile.all.each do |profile|
-	  define_singleton_method profile.name.to_sym do
-  		Profile.where(name: profile.name.to_sym).first
-  	end
-	end
+	
 end
