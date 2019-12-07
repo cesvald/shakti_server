2b163195a7fd8a06818e32e2b19cc25a709c1201
Cesar Valderrama <valderramago@gmail.com>
2019-12-07 23:32:56 +0000

    post_params in controller changes type by kind

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :excerpt, :excerpt_image, :body_image, :post_on, :type, :written_on)
+      params.require(:post).permit(:title, :body, :excerpt, :excerpt_image, :body_image, :post_on, :kind, :written_on)
     end
     
   
